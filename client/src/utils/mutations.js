4b5cbdfac550d5fdaf12240ffc55af401453d269
Wir3s <113479857+Wir3s@users.noreply.github.com>
2023-04-02 18:25:51 -0500

    Still not working

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -26,14 +26,34 @@ export const CREATE_USER = gql`
 `;
 
 export const SAVE_BOOK = gql`
-  mutation saveBook($bookId: String!) {
-    saveBook(bookId: $bookId) {
-      authors
-      bookId
-      description
-      image
-      link
-      title
+  mutation saveBook(
+    $bookId: String!
+    $description: String!
+    $title: String!
+    $authors: [String]
+    $image: String
+    $link: String
+  ) {
+    saveBook(
+      bookId: $bookId
+      description: $description
+      title: $title
+      authors: $authors
+      image: $image
+      link: $link
+    ) {
+      _id
+      bookCount
+      email
+      mybooks {
+        authors
+        bookId
+        description
+        image
+        link
+        title
+      }
+      username
     }
   }
 `;
